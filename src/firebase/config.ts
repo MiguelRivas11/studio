5b5fbf8e5c0fb3eaea48842978008013c9937066
MIGUEL ANGEL RIVAS GARCIA <2202018@universidad-une.com>
2025-10-07 15:29:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6459348889-e06b8",
+  "appId": "1:162241318169:web:5149e663c00925aa3e0988",
+  "apiKey": "AIzaSyCDwgJvXje55YSWe7PwreG0VQnjNkYGDdk",
+  "authDomain": "studio-6459348889-e06b8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "162241318169"
+};
